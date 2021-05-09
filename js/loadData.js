7da1fe31252884bcf4f15fc865d1c213447f4185
krullmizter <samuelgranvik@protonmail.com>
2021-05-09 13:50:52 +0300

    Changed d3.csv to use then and catch promises to load the CSV, and handle errors.

--- js/loadData.js
+++ js/loadData.js
@@ -2,13 +2,14 @@ $('document').ready(() => {
 
     function loadTweets() {
         const trumpTweets = 'data/trump_tweets.csv';
-        let trumpArray    = [];
-    
-        d3.csv(trumpTweets, (tweets) => {
-            trumpArray.push(tweets);
-        });
-    
-        console.info('Trump Tweets \n', trumpArray);
+
+        d3.csv(trumpTweets)
+          .then((tweets) => {
+                console.info('Trump Tweets \n', tweets);
+          })
+          .catch((err) => {
+                console.warn('Error loading tweets', err);
+          });
     }
 
     loadTweets();
