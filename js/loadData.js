f9a4259a8b5ca1eab139e6a44992e5758233551c
krullmizter <samuelgranvik@protonmail.com>
2021-05-09 13:30:44 +0300

    Initial work on loading data from .csv file.

--- js/loadData.js
+++ js/loadData.js
@@ -1,5 +1,16 @@
 $('document').ready(() => {
 
+    function loadTweets() {
+        const trumpTweets = 'data/trump_tweets.csv';
+        let trumpArray    = [];
     
+        d3.csv(trumpTweets, (tweets) => {
+            trumpArray.push(tweets);
+        });
+    
+        console.info('Trump Tweets \n', trumpArray);
+    }
+
+    loadTweets();
 
 });
