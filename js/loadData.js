c7042dcbc32917054b24cf80dd34f20cc136e4da
krullmizter <samuelgranvik@protonmail.com>
2021-05-09 20:15:42 +0300

    Began work on circular packing graph - Created the drawGraph function to handle the graph output. - The loadTweets function loops through the tweets and pushes to separate arrays for each CSV heading. - Began work on the basic structure and styling of each circle. - Working scaleOrdinal taking in the trump tweet targets and insults. - The above mentioned data applys size and colors to the circles based on the freq. of the given data. - Working size and coloring based on the data provided by the tweet data.

--- js/loadData.js
+++ js/loadData.js
@@ -1,17 +1,66 @@
 $('document').ready(() => {
+      let targets = [],
+          insults = [];
 
-    function loadTweets() {
-        const trumpTweets = 'data/trump_tweets.csv';
+      loadTweets();
 
-        d3.csv(trumpTweets)
-          .then((tweets) => {
-                console.info('Trump Tweets \n', tweets);
-          })
-          .catch((err) => {
-                console.warn('Error loading tweets', err);
-          });
-    }
+      function loadTweets() {
+          const trumpTweets = 'data/trump_tweets.csv';
 
-    loadTweets();
+          d3.csv(trumpTweets)
+            .then((tweets) => {
+                  console.info('Trump Tweets 🇺🇸 \n', tweets);
+                  
+                  for (let i = 0; i < tweets.length; i++) {
+                        targets.push(tweets[i].target);
+                        insults.push(tweets[i].insult);
+                  }
+
+                  drawGraph(tweets);
+            })
+            .catch((err) => {
+                  console.warn('Error loading tweets', err);
+            });      
+      }
+
+      function drawGraph(tweets) {
+            const width = 500, height = 500, sizes = [5, 75];
+            
+            const colors = [
+                  '#0275d8', 
+                  '#5cb85c',
+                  '#5bc0de',
+                  '#f0ad4e',
+                  '#d9534f',
+                  '#292b2c'
+            ];
+
+            const svg = d3.select('#graph')
+              .append('svg')
+                .attr('width', width)
+                .attr('height', height)
+
+            const size = d3.scaleOrdinal()
+              .domain(targets)
+              .range(sizes)
+
+            const color = d3.scaleOrdinal()
+              .domain(insults)
+              .range(colors)
+            
+            svg.selectAll('circle')
+               .data(tweets)
+               .enter()
+                 .append('circle')
+                 .classed('circle', true)
+                 .attr('cx', width  / 2)
+                 .attr('cy', height / 2)
+                 .attr('r',    (data) => { return size(data.target) })
+                 .attr('fill', (data) => { return color(data.insult) })
+                 .attr('stroke', 'black')
+                 .attr('stroke-width', 2);
+      }
 
 });
+
+      
